2664d3865f0092e73c823e6c725fae0038c4947e
Wojciech Kryg <krygwojciech@gmail.com>
2018-04-20 18:48:04 +0200

    Init commit

---
+++ app/src/main/java/com/findly/FindlyGlideModule.kt
@@ -0,0 +1,11 @@
+package com.findly
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Created by Wojdor on 20.04.2018.
+ */
+
+@GlideModule
+class FindlyGlideModule : AppGlideModule()
