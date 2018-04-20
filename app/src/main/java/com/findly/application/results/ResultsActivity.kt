15601ede6daa75d4dba599bce2c542acef4cd3c4
Wojciech Kryg <krygwojciech@gmail.com>
2018-04-21 01:41:14 +0200

    Added tags

--- app/src/main/java/com/findly/application/results/ResultsActivity.kt
+++ app/src/main/java/com/findly/application/results/ResultsActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.LinearSnapHelper
 import com.findly.R
 import com.findly.application.base.BaseActivity
 import com.findly.data.service.response.Offers
@@ -16,12 +18,14 @@ class ResultsActivity : BaseActivity(), ResultsContract.View {
     }
 
     var presenter: ResultsContract.Presenter = ResultsPresenter()
-    private val adapter: ResultsAdapter = ResultsAdapter()
+    private val resultsAdapter: ResultsAdapter = ResultsAdapter()
+    private val tagsAdapter: TagsAdapter = TagsAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_results)
         setupResultsRv()
+        setupTagsRv()
         presenter.attachView(this)
         // TODO: Change phrase
         presenter.downloadOffers("iphone")
@@ -29,8 +33,8 @@ class ResultsActivity : BaseActivity(), ResultsContract.View {
 
     private fun setupResultsRv() {
         activityResultsOffersRv.setLayoutManager(GridLayoutManager(this, NUMBER_OF_COLUMN))
-        activityResultsOffersRv.adapter = adapter
-        adapter.onItemClick { openOffer(it.url) }
+        activityResultsOffersRv.adapter = resultsAdapter
+        resultsAdapter.onItemClick { openOffer(it.url) }
     }
 
     private fun openOffer(url: String) {
@@ -39,7 +43,16 @@ class ResultsActivity : BaseActivity(), ResultsContract.View {
         startActivity(intent)
     }
 
+    private fun setupTagsRv() {
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        activityResultsTagsRv.layoutManager = layoutManager
+        activityResultsTagsRv.adapter = tagsAdapter
+        LinearSnapHelper().attachToRecyclerView(activityResultsTagsRv)
+        // TODO: Change tag list
+        tagsAdapter.updateTags(listOf("test0", "test1", "test2", "test3", "test4", "test5", "test6", "test7"))
+    }
+
     override fun showOffers(offers: List<Offers>) {
-        adapter.updateOffers(offers)
+        resultsAdapter.updateOffers(offers)
     }
 }
