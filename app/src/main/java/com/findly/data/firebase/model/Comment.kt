bd2a4dd9eba6c1a812552fe5e8e688356c0386c6
Wojciech Kryg <krygwojciech@gmail.com>
2018-04-21 04:52:47 +0200

    Added adding and getting data from Firebase

--- app/src/main/java/com/findly/data/firebase/model/Comment.kt
+++ app/src/main/java/com/findly/data/firebase/model/Comment.kt
@@ -3,7 +3,9 @@ package com.findly.data.firebase.model
 /**
  * Created by Wojdor on 21.04.2018.
  */
-data class Comment(var userName: String,
-                   var content: String,
-                   var idAuction: String,
-                   var isAccepted: Boolean)
+class Comment {
+    var userName: String = ""
+    var content: String = ""
+    var idAuction: String = ""
+    var isAccepted: Boolean = false
+}
