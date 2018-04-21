7d497efb7afecdd56d6c8e9c60d4370fa65701d1
Patryk Gałązka <patryk.galazka@euvic.pl>
2018-04-21 16:06:38 +0200

    lottie changed

--- app/src/main/java/com/findly/application/main/unrecognised/UnrecognisedFragment.kt
+++ app/src/main/java/com/findly/application/main/unrecognised/UnrecognisedFragment.kt
@@ -3,6 +3,9 @@ package com.findly.application.main.recognised
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +14,7 @@ import com.findly.application.base.BaseFragment
 import com.findly.application.main.unrecognised.UnrecognisedAdapter
 import com.findly.application.postDetail.PostDetailsActivity
 import com.findly.data.firebase.Database
+import com.findly.data.firebase.model.Post
 import kotlinx.android.synthetic.main.fragment_unrecognised.*
 
 /**
@@ -22,6 +26,9 @@ class UnrecognisedFragment : BaseFragment(), UnrecognisedContract.View {
     companion object {
         private const val NUMBER_OF_COLUMN = 3
     }
+
+    var searchPhrase: String = ""
+    var posts = mutableListOf<Post>()
     var presenter: UnrecognisedContract.Presenter = UnrecognisedPresenter()
     private val adapter = UnrecognisedAdapter()
 
@@ -30,7 +37,7 @@ class UnrecognisedFragment : BaseFragment(), UnrecognisedContract.View {
     }
 
     private fun setupPostsRv() {
-        fragmentUnrecognisedPostsRv.layoutManager = GridLayoutManager(context, NUMBER_OF_COLUMN)
+        fragmentUnrecognisedPostsRv.layoutManager = GridLayoutManager(context, NUMBER_OF_COLUMN) as RecyclerView.LayoutManager?
         fragmentUnrecognisedPostsRv.adapter = adapter
         adapter.onItemClick {
             startActivity(Intent(context, PostDetailsActivity::class.java).apply {
@@ -43,6 +50,22 @@ class UnrecognisedFragment : BaseFragment(), UnrecognisedContract.View {
         super.onViewCreated(view, savedInstanceState)
         presenter.attachView(this)
         setupPostsRv()
-        Database.getPosts { adapter.updatePosts(it) }
+        Database.getPosts {
+            adapter.updatePosts(it.filter { it.description.contains(searchPhrase.toRegex()) })
+            posts = it
+        }
+        fragmentUnrecognisedSearchEt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(text: CharSequence, p1: Int, p2: Int, p3: Int) {
+                searchPhrase = text.toString()
+                adapter.updatePosts(posts.filter { it.description.contains(text.toString().toRegex()) })
+            }
+
+        })
     }
 }
